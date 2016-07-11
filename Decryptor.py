5a800d869b04e1680624b9fde22e768b3004fceb
19thhell <yangxh92@gmail.com>
2016-07-10 22:24:12 -0700

    verbose info

--- Decryptor.py
+++ Decryptor.py
@@ -7,16 +7,26 @@ from pathlib import Path
 from pathlib import PurePath
 from os import path
 
+def not_empty(string):
+    return string != None and string != ''
+
+def error_info(title, msg):
+    messagebox.showinfo(title, msg)
+    return 'Program abort'
+
 dest_root = tkinter.Tk()
 dest_root.withdraw()
 destination = filedialog.askdirectory(parent=dest_root, title='Choose destination directory')
+assert not_empty(destination), error_info('Error', 'Destination directory not selected')
 
 files_root = tkinter.Tk()
 files_root.withdraw()
 files = filedialog.askopenfilenames(parent=files_root, title='Choose files to be decrypted')
+assert not_empty(files), error_into('Error', 'Files to be decrypted not selected')
 to_decrypt_list = files_root.tk.splitlist(files)
 
 key = simpledialog.askstring('Password', 'Enter password: ', show='*')
+assert not_empty(key), error_info('Error', 'Password not entered')
 des = pyDes.des(key, padmode=pyDes.PAD_PKCS5)
 
 for to_decrypt in to_decrypt_list:
--- Encryptor.py
+++ Encryptor.py
@@ -3,25 +3,36 @@ import tkinter
 import config
 from tkinter import filedialog
 from tkinter import simpledialog
+from tkinter import messagebox
 from pathlib import Path
 from pathlib import PurePath
 from os import path
 
+def not_empty(string):
+    return string != None and string != ''
+
+def error_info(title, msg):
+    messagebox.showinfo(title, msg)
+    return 'Program abort'
+
 dest_root = tkinter.Tk()
 dest_root.withdraw()
 destination = filedialog.askdirectory(parent=dest_root, title='Choose destination directory')
+assert not_empty(destination), error_info('Error', 'Destination directory not selected')
 
 files_root = tkinter.Tk()
 files_root.withdraw()
 files = filedialog.askopenfilenames(parent=files_root, title='Choose files to be encrypted')
+assert not_empty(files), error_into('Error', 'Files to be encrypted not selected')
 to_encrypt_list = files_root.tk.splitlist(files)
 
-key = simpledialog.askstring('Password', 'Enter password: ', show='*')
+key = simpledialog.askstring('Password', 'Enter password (8 characters): ', show='*')
+assert not_empty(key), error_info('Error', 'Password not entered')
+assert len(key) == 8, error_info('Error', 'Password is not 8 characters')
 des = pyDes.des(key, padmode=pyDes.PAD_PKCS5)
 
 for to_encrypt in to_encrypt_list:
     output_path = path.join(destination, config.prefix + PurePath(to_encrypt).name)
-    print(output_path)
     with open(output_path, 'wb') as encrypted:
         with open(to_encrypt, 'rb') as content:
             encrypted.write(des.encrypt(content.read()))
