0c2ba75dd302f8d2c684c8e8d7ce91a4a06be97f
19thhell <yangxh92@gmail.com>
2016-07-10 23:00:25 -0700

    bug fixed, progress bar

--- Decryptor.py
+++ Decryptor.py
@@ -14,6 +14,9 @@ def error_info(title, msg):
     messagebox.showinfo(title, msg)
     return 'Program abort'
 
+def print_no_newline(string):
+    print(string, end='', flush=True)
+
 dest_root = tkinter.Tk()
 dest_root.withdraw()
 destination = filedialog.askdirectory(parent=dest_root, title='Choose destination directory')
@@ -29,8 +32,24 @@ key = simpledialog.askstring('Password', 'Enter password: ', show='*')
 assert not_empty(key), error_info('Error', 'Password not entered')
 des = pyDes.des(key, padmode=pyDes.PAD_PKCS5)
 
+prev_length = 0
+unit = 1 / len(to_decrypt_list)
+total = 0
+
+progress = '=' * int(total * 100) + '>' + ' {:.2%}'.format(total)
+print_no_newline(progress)
+
 for to_decrypt in to_decrypt_list:
-    output_path = path.join(destination, PurePath(to_decrypt).name[len(config.name) : ])
+    output_path = path.join(destination, PurePath(to_decrypt).name[len(config.prefix) : ])
     with open(output_path, 'wb') as decrypted:
         with open(to_decrypt, 'rb') as content:
             decrypted.write(des.decrypt(content.read()))
+        prev_length = len(progress)
+        total += unit
+    print_no_newline('\b' * prev_length)
+    progress = '=' * int(total * 100) + '>' + ' {:.2%}'.format(total)
+    print_no_newline(progress)
+print('\nDecryption completed')
+
+dest_root.destroy()
+files_root.destroy()
--- Encryptor.py
+++ Encryptor.py
@@ -15,6 +15,9 @@ def error_info(title, msg):
     messagebox.showinfo(title, msg)
     return 'Program abort'
 
+def print_no_newline(string):
+    print(string, end='', flush=True)
+
 dest_root = tkinter.Tk()
 dest_root.withdraw()
 destination = filedialog.askdirectory(parent=dest_root, title='Choose destination directory')
@@ -31,8 +34,24 @@ assert not_empty(key), error_info('Error', 'Password not entered')
 assert len(key) == 8, error_info('Error', 'Password is not 8 characters')
 des = pyDes.des(key, padmode=pyDes.PAD_PKCS5)
 
+prev_length = 0
+unit = 1 / len(to_encrypt_list)
+total = 0
+
+progress = '=' * int(total * 100) + '>' + ' {:.2%}'.format(total)
+print_no_newline(progress)
+
 for to_encrypt in to_encrypt_list:
     output_path = path.join(destination, config.prefix + PurePath(to_encrypt).name)
     with open(output_path, 'wb') as encrypted:
         with open(to_encrypt, 'rb') as content:
             encrypted.write(des.encrypt(content.read()))
+        prev_length = len(progress)
+        total += unit
+    print_no_newline('\b' * prev_length)
+    progress = '=' * int(total * 100) + '>' + ' {:.2%}'.format(total)
+    print_no_newline(progress)
+print('\nEncryption completed')
+
+dest_root.destroy()
+files_root.destroy()
